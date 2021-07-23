eca13126843f7f5a8fa5fb1a76ce08dacf95958a
Diana Thayer <garbados@gmail.com>
2021-07-23 10:17:57 -0700

    chore: use pbkdf2 per transform

--- index.js
+++ index.js
@@ -1,31 +1,52 @@
 const { secretbox, hash, randomBytes } = require('tweetnacl')
 const { decodeUTF8, encodeUTF8, encodeBase64, decodeBase64 } = require('tweetnacl-util')
+const { pbkdf2 } = require('pbkdf2')
 
 const NO_PASSWORD = 'A password is required for encryption or decryption.'
 const COULD_NOT_DECRYPT = 'Could not decrypt!'
 
+const SALT_LENGTH = secretbox.nonceLength
+const KEY_LENGTH = secretbox.keyLength
+const ITERATIONS = 1e3
+const HASH = 'sha512'
+
 module.exports = class Crypt {
+  static async deriveKey (password, salt) {
+    if (!salt) { salt = randomBytes(SALT_LENGTH) }
+    const key = await new Promise((resolve, reject) => {
+      pbkdf2(password, salt, ITERATIONS, KEY_LENGTH, HASH, (err, key) => {
+        /* istanbul ignore next */
+        if (err) { return reject(err) } else { return resolve(key) }
+      })
+    })
+    return { key, salt }
+  }
+
   constructor (password) {
     if (!password) { throw new Error(NO_PASSWORD) }
-    this._key = hash(decodeUTF8(password)).slice(0, secretbox.keyLength)
+    this._pass = hash(decodeUTF8(password)).slice(0, KEY_LENGTH)
   }
 
   async encrypt (plaintext) {
+    const { key, salt } = await Crypt.deriveKey(this._pass)
     const nonce = randomBytes(secretbox.nonceLength)
     const messageUint8 = decodeUTF8(plaintext)
-    const box = secretbox(messageUint8, nonce, this._key)
-    const fullMessage = new Uint8Array(nonce.length + box.length)
-    fullMessage.set(nonce)
-    fullMessage.set(box, nonce.length)
+    const box = secretbox(messageUint8, nonce, key)
+    const fullMessage = new Uint8Array(salt.length + nonce.length + box.length)
+    fullMessage.set(salt)
+    fullMessage.set(nonce, salt.length)
+    fullMessage.set(box, salt.length + nonce.length)
     const base64FullMessage = encodeBase64(fullMessage)
     return base64FullMessage
   }
 
   async decrypt (messageWithNonce) {
     const messageWithNonceAsUint8Array = decodeBase64(messageWithNonce)
-    const nonce = messageWithNonceAsUint8Array.slice(0, secretbox.nonceLength)
-    const message = messageWithNonceAsUint8Array.slice(secretbox.nonceLength)
-    const decrypted = secretbox.open(message, nonce, this._key)
+    const salt = messageWithNonceAsUint8Array.slice(0, SALT_LENGTH)
+    const nonce = messageWithNonceAsUint8Array.slice(SALT_LENGTH, SALT_LENGTH + secretbox.nonceLength)
+    const message = messageWithNonceAsUint8Array.slice(SALT_LENGTH + secretbox.nonceLength)
+    const { key } = await Crypt.deriveKey(this._pass, salt)
+    const decrypted = secretbox.open(message, nonce, key)
     if (!decrypted) {
       throw new Error(COULD_NOT_DECRYPT)
     } else {
--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 /* global describe, it */
 const assert = require('assert').strict
+const { encodeBase64 } = require('tweetnacl-util')
 const Crypt = require('.')
 
 const PLAINTEXT = 'hello world'
@@ -7,6 +8,28 @@ const PASSWORD = 'password'
 const BENCHMARK = 1e4 // note: 1e4 = 1 and 4 zeroes (10,000)
 
 describe('crypt', function () {
+  it('should derive a key from a password', async function () {
+    let { key, salt } = await Crypt.deriveKey(PASSWORD)
+    key = encodeBase64(key)
+    assert.equal(typeof key, 'string')
+    let { key: key2 } = await Crypt.deriveKey(PASSWORD, salt)
+    key2 = encodeBase64(key2)
+    assert.equal(key, key2)
+  })
+
+  it('should require a password', function () {
+    let ok = false
+    try {
+      const crypt = new Crypt()
+      throw new Error(`crypt created: ${!!crypt}`)
+    } catch (error) {
+      if (error.message === 'A password is required for encryption or decryption.') {
+        ok = true
+      }
+    }
+    assert(ok)
+  })
+
   it('should do the crypto dance', async function () {
     const crypt = new Crypt(PASSWORD)
     const ciphertext = await crypt.encrypt(PLAINTEXT)
@@ -29,7 +52,7 @@ describe('crypt', function () {
   })
 
   it(`should do the crypto dance ${BENCHMARK} times`, async function () {
-    this.timeout(BENCHMARK) // assume each op will take no more than 1ms
+    this.timeout(BENCHMARK * 10) // assume each op will take no more than 10ms
     const crypt = new Crypt(PASSWORD)
     for (let i = 0; i < BENCHMARK; i++) {
       const ciphertext = await crypt.encrypt(PLAINTEXT)
